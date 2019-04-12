0c52391e4e5e80cc6c19f483efcd0f814286fd44
Aninda Sarker Rahul <rahul108sk@gmail.com>
2019-04-12 18:44:41 +0600

    Create Print_a_character_n_times_without_using_loop_recursion_or_goto.cpp

---
+++ Print_a_character_n_times_without_using_loop_recursion_or_goto.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void prn(char c, int n){
+    cout<<string(n,c)<<endl;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    char c;
+    cin>>c;
+    prn(c,n);
+    return 0;
+}
