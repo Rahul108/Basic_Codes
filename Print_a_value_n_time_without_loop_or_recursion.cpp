901a5088d64e094cc9419be5bb11e0f78bfcb887
Aninda Sarker Rahul <rahul108sk@gmail.com>
2019-04-12 18:40:08 +0600

    Create Print_a_value_n_time_without_loop_or_recursion.cpp

---
+++ Print_a_value_n_time_without_loop_or_recursion.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class print{
+public:
+    print(){
+        cout<<"Text"<<endl;
+    }
+};
+
+int main()
+{
+    int n;
+    cin>>n;
+    print a[n];
+    return 0;
+}
